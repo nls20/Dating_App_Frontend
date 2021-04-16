94c85d55f9e1dccd7ed9f435d3958c1062e81757
BrianBell422 <brianbell.uk@gmail.com>
2021-04-16 13:43:20 +0100

    rough draft of swiping page done

--- src/components/SwipingPage.js
+++ src/components/SwipingPage.js
@@ -2,21 +2,43 @@ import {useState, useEffect} from 'react'
 
 const SwipingPage = ({potentialMatches}) => {
 
-    // const [potentialMatches, setPotentialMatches] = useState(potentialMatches)
     const [matches, setMatches] = useState([])
     const [notMatched, setNotMatched] = useState([])
 
-    const handleMatchYes = (evt) => {
-
+    const selectOption = () => {
+        const option = potentialMatches[Math.floor(Math.random() * potentialMatches.length)]
     }
+
+    const handleMatchYes = (evt) => {
+        setMatches([...matches] + evt.target.value)
+        removePotentialMatches(option.id)
+        selectOption()
+    }
+
+    const removePotentialMatches = (idToRemove) => {
+        setPotentialMatches(potentialMatches.filter(({ id }) => id !== idToRemove))
+      }
 
     const handleMatchNo = (evt) => {
-        
+        removePotentialMatches(option.id)
+        setNotMatched([...notMatched] + evt.target.value)
+        selectOption()
     }
+
+    useEffect(() => {
+        selectOption()
+    }, [potentialMatches])
 
     return (
         <>
+        <div>
+            <p>{option.img}</p>
+            <p>{option.name}</p>
+            <p>{option.hobbies}</p>
+        </div>
+
         <button onClick={handleMatchYes}>YES</button>
+        
         <button onClick={handleMatchNo}>NO</button>
         </>
     )
