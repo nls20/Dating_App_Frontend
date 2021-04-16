3e503786c4828d77f3329b91d70222783e28c4dc
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-16 14:13:06 +0100

    moved matches to container

--- src/components/Matches/MatchesPage.js
+++ src/components/Matches/MatchesPage.js
@@ -1,19 +1,9 @@
 import {useState} from 'react'
 import MatchesPreviewTiles from './MatchesPreviewTiles'
 import MessagesPreview from './MessagesPreview'
-const MatchesPage = () => {
+const MatchesPage = ({matches}) => {
 
-    const matches = [
-        {name: "andrew", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message "},
-        {name: "david", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a david message less than 60 characters"},
-        {name: "mark", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a mark message more than 60 characters so some of it will not appear"},
-        {name: "nicola", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a nicola message"},
-        {name: "brian", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a brian message"},
-        {name: "andrew", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-        {name: "david", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-        {name: "mark", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-        {name: "nicola", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"}
-    ]
+    
 
 
     
