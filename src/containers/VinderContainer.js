0bdec46b89af656cd8c94f9d5c6e0398e5325872
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-17 11:52:28 +0100

    nav bar fixed to bottom of screen

--- src/containers/VinderContainer.js
+++ src/containers/VinderContainer.js
@@ -1,57 +1,112 @@
-import {useState} from 'react'
-import {BrowserRouter  as Router, Route, Switch} from 'react-router-dom'
-import SwipingPage from '../components/SwipingPage'
-import NavBar from '../components/NavBar/NavBar'
-import MatchesPage from '../components/Matches/MatchesPage'
-import ProfilePage from '../components/Profile/ProfilePage'
-import Chatroom from '../components/Conversation/Chatroom'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SwipingPage from "../components/SwipingPage";
+import NavBar from "../components/NavBar/NavBar";
+import MatchesPage from "../components/Matches/MatchesPage";
+import ProfilePage from "../components/Profile/ProfilePage";
+import Chatroom from "../components/Conversation/Chatroom";
+import "./VinderContainer.css";
 
 const VinderContainer = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [location, setLocation] = useState("");
+  const [hobbies, setHobbies] = useState("");
+  const [vaccinated, setVaccinated] = useState(false);
 
-    const [name, setName] = useState("");
-    const [age, setAge] = useState(0);
-    const [location, setLocation] = useState("");
-    const [hobbies, setHobbies] = useState("");
-    const [vaccinated, setVaccinated] = useState(false);
+  const matches = [
+    {
+      name: "andrew",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory:
+        "this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message ",
+    },
+    {
+      name: "david",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a david message less than 60 characters",
+    },
+    {
+      name: "mark",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory:
+        "this is a mark message more than 60 characters so some of it will not appear",
+    },
+    {
+      name: "nicola",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a nicola message",
+    },
+    {
+      name: "brian",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a brian message",
+    },
+    {
+      name: "andrew",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a message",
+    },
+    {
+      name: "david",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a message",
+    },
+    {
+      name: "mark",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a message",
+    },
+    {
+      name: "nicola",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      messageHistory: "this is a message",
+    },
+  ];
 
-    const matches = [
-      {name: "andrew", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message "},
-      {name: "david", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a david message less than 60 characters"},
-      {name: "mark", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a mark message more than 60 characters so some of it will not appear"},
-      {name: "nicola", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a nicola message"},
-      {name: "brian", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a brian message"},
-      {name: "andrew", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-      {name: "david", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-      {name: "mark", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"},
-      {name: "nicola", picture: "https://i.redd.it/v0caqchbtn741.jpg", messageHistory: "this is a message"}
-    ]
+  const potentialMatches = [
+    {
+      name: "john",
+      age: 30,
+      location: "Inverness",
+      hobbies: "running",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+    },
+    {
+      name: "bill",
+      age: 50,
+      location: "Glasgow",
+      hobbies: "swimming",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+    },
+    {
+      name: "mike",
+      age: 45,
+      location: "Edinburgh",
+      hobbies: "cycling",
+      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+    },
+  ];
 
+  const getFormInformation = (details) => {
+    setName(details.name);
+    setAge(details.age);
+    setLocation(details.location);
+    setHobbies(details.hobbies);
+    setVaccinated(details.vaccinated);
+  };
 
-    const potentialMatches = [
-      {name: "john", age: 30, location: "Inverness", hobbies: "running", picture:"https://i.redd.it/v0caqchbtn741.jpg"},
-      {name: "bill", age: 50, location: "Glasgow", hobbies: "swimming", picture:"https://i.redd.it/v0caqchbtn741.jpg"},
-      {name: "mike", age: 45, location: "Edinburgh", hobbies: "cycling", picture:"https://i.redd.it/v0caqchbtn741.jpg"}
-    ]
-
-
-    const getFormInformation = (details) => {
-        setName(details.name)
-        setAge(details.age)
-        setLocation(details.location)
-        setHobbies(details.hobbies)
-        setVaccinated(details.vaccinated)
-    }
-
-    return (
-        <Router>
-        
-          <Switch id="">
+  return (
+    <Router>
+      <section id="router">
+        <div>
+          <Switch id="switch">
             <Route exact path="/">
               <SwipingPage potentialMatches={potentialMatches} />
             </Route>
 
             <Route exact path="/matches">
-              <MatchesPage matches={matches}/>
+              <MatchesPage matches={matches} />
             </Route>
 
             <Route path="/profile">
@@ -59,12 +114,15 @@ const VinderContainer = () => {
             </Route>
 
             <Route path="/matches/conversation">
-              <Chatroom/>
+              <Chatroom />
             </Route>
-          
           </Switch>
-          <NavBar/>
-      </Router>
-    )
-}
-export default VinderContainer
+        </div>
+        <div id="nav-bar">
+          <NavBar />
+        </div>
+      </section>
+    </Router>
+  );
+};
+export default VinderContainer;
