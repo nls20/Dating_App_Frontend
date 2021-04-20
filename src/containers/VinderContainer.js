b3c0ae3217dc016b158d867813025a72dfead00c
nls20 <nls20@hotmail.co.uk>
2021-04-20 09:41:03 +0100

    edited images in matches

--- src/containers/VinderContainer.js
+++ src/containers/VinderContainer.js
@@ -19,24 +19,24 @@ const VinderContainer = () => {
   const matches = [
     {
       name: "Andrew",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://i.pinimg.com/originals/4e/10/e5/4e10e564d614436de1fc60cb45198cc4.jpg",
       messageHistory:
         "this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message this is andrew message ",
     },
     {
       name: "David",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       messageHistory: "this is a david message less than 60 characters",
     },
     {
       name: "Mark",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHByb2ZpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
       messageHistory:
         "this is a mark message more than 60 characters so some of it will not appear",
     },
     {
       name: "Nicola",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://image.freepik.com/free-photo/girl-cafe-with-smartphone_1321-389.jpg",
       messageHistory: "this is a nicola message",
     },
     {
@@ -46,24 +46,24 @@ const VinderContainer = () => {
     },
     {
       name: "Andrew",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://i.pinimg.com/originals/4e/10/e5/4e10e564d614436de1fc60cb45198cc4.jpg",
       messageHistory: "this is a message",
     },
     {
       name: "David",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       messageHistory: "this is a message",
     },
     {
       name: "Mark",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHByb2ZpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
       messageHistory: "this is a message",
     },
     {
       name: "Nicola",
-      picture: "https://i.redd.it/v0caqchbtn741.jpg",
+      picture: "https://image.freepik.com/free-photo/girl-cafe-with-smartphone_1321-389.jpg",
       messageHistory: "this is a message",
-    },
+    }
   ];
 
   // const potentialMatches = [
