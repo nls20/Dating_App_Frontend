f0df07d5a6f52da68eb5fe8895f41a078cf386e4
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-22 12:41:40 +0100

    message preview now shows name and last messages

--- src/components/Matches/MessageTemplateTile.js
+++ src/components/Matches/MessageTemplateTile.js
@@ -1,51 +1,42 @@
-import './MessageTemplateTile.css'
+import "./MessageTemplateTile.css";
 
-const MessageTemplateTile = ({match}) => {
+const MessageTemplateTile = ({ match }) => {
+  console.log("match", match);
 
-    console.log('match', match);
+  //let messages = []
 
-    //let messages = []
+  //for (int i=0;i<match.length;i++){
+  // if (match[i].fromUser.id === 8){
+  // messages.push({person: "current", message: match[i].message})
+  // } else if(match[i].fromUser.id !== 8) {
+  // messages.push({person: "target", message: match[i].message})
+  // }
+  // }
+  let matchName;
+  let picture;
+  for (let i = 0; i < match.length; i++) {
+    if (match[i].fromUser.name !== "Barry") {
+      matchName = match[i].fromUser.name;
+    //   picture = match[i].fromUser.picture
+    }
+  }
 
-    //for (int i=0;i<match.length;i++){
-        // if (match[i].fromUser.id === 8){
-            // messages.push({person: "current", message: match[i].message})
-        // } else if(match[i].fromUser.id !== 8) {
-            // messages.push({person: "target", message: match[i].message})
-        // }
-    // }
-    let matchName = ""
-    
-    let messagePreview = match[match.length-1].message
-    // const maxLength = 60
+//   let messagePreview = match[match.length - 1].message;
+//   const maxLength = 60
 
-    // if (match.user.toMessages.length > 0){
-    //     messagePreview=match.user.toMessages[0].message
-    // } else if (match.user.fromMessages> 0){
-    //     messagePreview=match.user.fromMessages[0].message
-    // } else {
-    //     // don't show the message tab
-    // }
+  
 
-    // console.log('message preveiw', messagePreview);
+  return (
+    <section id="total-tile-section">
+      <section id="tile-picture-section">
+        {/* <img src={match.picture} /> */}
+      </section>
 
-    // if (match.messages.length > maxLength){
-    //     messagePreview = match.message.substring(0, maxLength) + "..."
-    // } 
-
-    return (
-        <section id="total-tile-section">
-
-            <section id="tile-picture-section">
-                {/* <img src={match.picture} /> */}
-            </section>
-
-            <section id="tile-section">
-                {/* <h5>{matchName}</h5> */}
-                <p>{messagePreview}</p>
-            </section>
-
-        </section>
-        
-    )
-}
-export default MessageTemplateTile
+      <section id="tile-section">
+        <h5>{matchName}</h5>
+        <p>{messagePreview}</p>
+      </section>
+    </section>
+  );
+};
+export default MessageTemplateTile;
