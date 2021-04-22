b46c27ff06fd71d47bd47b9b0527bb06eb660a00
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-22 12:14:43 +0100

    added somethign

--- src/components/Matches/MessageTemplateTile.js
+++ src/components/Matches/MessageTemplateTile.js
@@ -4,32 +4,45 @@ const MessageTemplateTile = ({match}) => {
 
     console.log('match', match);
 
-    let messagePreview = ""
-    const maxLength = 60
+    //let messages = []
 
-    if (match.user.toMessages.length > 0){
-        messagePreview=match.user.toMessages[0].message
-    } else if (match.user.fromMessages> 0){
-        messagePreview=match.user.fromMessages[0].message
-    } else {
-        // don't show the message tab
-    }
+    //for (int i=0;i<match.length;i++){
+        // if (match[i].fromUser.id === 8){
+            // messages.push({person: "current", message: match[i].message})
+        // } else if(match[i].fromUser.id !== 8) {
+            // messages.push({person: "target", message: match[i].message})
+        // }
+    // }
+    let matchName = ""
+    match.find(m => {
+        if (m.fromUser)
+    })
+    let messagePreview = match[match.length]
+    // const maxLength = 60
 
-    console.log('message preveiw', messagePreview);
+    // if (match.user.toMessages.length > 0){
+    //     messagePreview=match.user.toMessages[0].message
+    // } else if (match.user.fromMessages> 0){
+    //     messagePreview=match.user.fromMessages[0].message
+    // } else {
+    //     // don't show the message tab
+    // }
 
-    if (match.messages.length > maxLength){
-        messagePreview = match.message.substring(0, maxLength) + "..."
-    } 
+    // console.log('message preveiw', messagePreview);
+
+    // if (match.messages.length > maxLength){
+    //     messagePreview = match.message.substring(0, maxLength) + "..."
+    // } 
 
     return (
         <section id="total-tile-section">
 
             <section id="tile-picture-section">
-                <img src={match.picture} />
+                {/* <img src={match.picture} /> */}
             </section>
 
             <section id="tile-section">
-                <h5>{match.user.name}</h5>
+                <h5>{matchName}</h5>
                 <p>{messagePreview}</p>
             </section>
 
